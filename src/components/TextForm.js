eacedc208fe4c09d16a187ba03d00e082a9bbbf9
Aakib-Ali <aakibali449@gmail.com>
2024-01-01 17:42:07 +0530

    Add buttons

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,6 +4,18 @@ function TextForm(props){
         let newText=text.toUpperCase();
         setText(newText);
     }
+    const handleLo = ()=>{
+        let newText=text.toLowerCase();
+        setText(newText);
+    }
+    const handleClear = ()=>{
+        let newText='';
+        setText(newText);
+    }
+    const handleSpace = ()=>{
+        let newText=text.split(" ").join("");
+        setText(newText);
+    }
 
     const handleOnChange =(event)=>{
         setText(event.target.value);
@@ -15,12 +27,19 @@ function TextForm(props){
             <div className="mb-3">
                 <textarea className="form-control" onChange={handleOnChange} placeholder='Enter your Text for conversion' value={text} id="box" rows="8"></textarea>
             </div>
-            <button className='btn btn-primary' onClick={handleUp}>Convert to Upper</button>
+            <button className='btn btn-primary mx-1' onClick={handleUp}>Convert to Upper</button>
+            <button className='btn btn-primary mx-1' onClick={handleLo}>Convert to Lower</button>
+            <button className='btn btn-primary mx-1' onClick={handleClear}>Clear Text</button>
+            <button className='btn btn-primary mx-1' onClick={handleSpace}>Remove spaces</button>
+
             <div className="container my-5">
                 <h1>Your Text summary</h1>
                     <p> {text.split(" ").length} words and {text.length} character int your text </p>
                     <p> {0.008*text.split(" ").length} minutes time for reading</p>
+                    <h1>Preveiw</h1>
+                    <p>{text}</p>
             </div>
+
         </>
     );
 }export default TextForm;
