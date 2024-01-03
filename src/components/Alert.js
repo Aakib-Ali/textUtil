d6747f550e4af086c079022e33fc49895e327beb
Aakib-Ali <aakibali449@gmail.com>
2024-01-03 16:20:33 +0530

    favicon

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,7 +2,9 @@ import React from 'react'//rfce
 
 function Alert(props) {
   return (
-    props.alert && <div className={`alert alert-${props.alert.type} alert-dismissable fade show`}role="alert">
+    props.alert && <div className={`alert alert-${props.alert.type} setTimeout(()->{
+        document.querySelector('.alert').remove();
+    }) alert-dismissable fade show`}role="alert">
         <strong>{props.alert.type}</strong>{props.alert.message}
     </div>
   )
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -23,18 +23,18 @@ function TextForm(props){
     const [text,setText]= useState('');
     return (
         <>
-            <h1 style={{color:props.mode==='white'?'dark':'white'}}>{props.heading}</h1>
+            <h1 style={{color:props.mode==='light'?'black':'white'}}>{props.heading}</h1>
             <div className="mb-3" style={{backgroundColor:props.mode==='dark'?'grey ':'white'}}>
-                <textarea className="form-control" onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey ':'white' , color:props.mode==='white'?'dark':'white'}} value={text} id="box" rows="8"></textarea>
+                <textarea className="form-control" onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey ':'white' , color:props.mode==='light'?'dark':'white'}} value={text} id="box" rows="8"></textarea>
             </div>
             <button className='btn btn-primary mx-1' onClick={handleUp}>Convert to Upper</button>
             <button className='btn btn-primary mx-1' onClick={handleLo}>Convert to Lower</button>
             <button className='btn btn-primary mx-1' onClick={handleClear}>Clear Text</button>
             <button className='btn btn-primary mx-1' onClick={handleExtraSpace}>Remove Extra spaces</button>
 
-            <div style={{color:props.mode==='dark'?'white ':'dark'}} className="container my-5" c>
+            <div style={{color:props.mode==='dark'?'white':'black'}} className="container my-5" c>
                 <h1 >Your Text summary</h1>
-                    <p > {text.split(" ").length} words and {text.length} character int your text </p>
+                    <p > {text.split(" ").length-1} words and {text.length} character int your text </p>
                     <p > {0.008*text.split(" ").length} minutes time for reading</p>
                     <h1 >Preveiw</h1>
                     <p >{text.length>0?text:"Enter something above text area for preview"}</p>
